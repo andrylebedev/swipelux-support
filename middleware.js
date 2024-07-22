34fc4c760f19cc47b54afdab276295e64c412d07
Аndry Lebedev <andry@Andrys-MacBook-Pro.local>
2024-07-22 22:03:48 +0200

    Added all projects files

--- middleware.js
+++ middleware.js
@@ -1,14 +1,9 @@
-import { next } from '@vercel/edge';
-
-export default function middleware(req) {
-  return next({
-    headers: {
-      'Referrer-Policy': 'origin-when-cross-origin',
-      'X-Frame-Options': 'DENY',
-      'X-Content-Type-Options': 'nosniff',
-      'X-DNS-Prefetch-Control': 'on',
-      'Strict-Transport-Security':
-        'max-age=31536000; includeSubDomains; preload',
-    },
-  });
-}
+module.exports = (req, res) => {
+  res.setHeader('Content-Security-Policy', 'default-src \'self\'; script-src \'self\' https://widget.intercom.io;');
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-DNS-Prefetch-Control', 'off');
+  res.setHeader('Referrer-Policy', 'no-referrer');
+  res.end('Middleware applied');
+};
